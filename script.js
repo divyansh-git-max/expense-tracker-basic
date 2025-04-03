817a5fdac4f5b5c53860ed6c6f2d8d3aa05f9bb3
divyansh-git-max <divyanshajay@gmail.com>
2025-04-04 01:27:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const expenseForm = document.getElementById('expense-form');
+    const expenseName = document.getElementById('expense-name');
+    const expenseAmount = document.getElementById("expense-amount");
+    const expenseList = document.getElementById('expense-list');
+    const totalAmountDisplay = document.getElementById('total-amount');
+    
+
+    let expense=[];
+    let totalAmount =
+
+})
