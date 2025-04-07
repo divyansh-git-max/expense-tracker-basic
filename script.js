6ee0ad136b5b4d73fb7eba56d33270b0f98d2b01
Divyansh Ajay <divyanshajay@gmail.com>
2025-04-07 21:13:28 +0530

    Add delete expenses Function

--- script.js
+++ script.js
@@ -6,31 +6,61 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalAmountDisplay = document.getElementById('total-amount');
     
 
-    let expenses=[]; 
+    let expenses= JSON.parse(localStorage.getItem('expenses'))||[];
     let totalAmount = calculateExpenseTotal();
-
+    renderExpenses();
+    updateTotal();
     expenseForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const name = expenseName.value.trim();
-        const amount = expenseAmount.value.trim();
+        const amount = parseFloat(expenseAmount.value.trim());
         
         if (name!=="" && !isNaN(amount) && amount>0) {
             const newExpense = {
                 id: Date.now(),
-                name,
-                amount,
+                name:name,
+                amount:amount,
             }
             expenses.push(newExpense);
-            
-        }
- 
+            saveExpensetoLocale(expenses);
+            renderExpenses();
+            updateTotal();
 
+            expenseName.value = '';
+            expenseAmount.value = '';
+            console.log(expenses);
+        }
     });
 
-    function calculateExpenseTotal() {
-
+    function saveExpensetoLocale(expenses) {
+        localStorage.setItem('expenses', JSON.stringify(expenses));
     }
-
-
+    function calculateExpenseTotal() {
+        return expenses.reduce((sum,expense) => sum + expense.amount, 0);
+    }
+    function updateTotal(){
+        totalAmount = calculateExpenseTotal();
+        console.log(totalAmount)
+        totalAmountDisplay.textContent = totalAmount.toFixed(2);
+    }
+    function renderExpenses() {
+        expenseList.innerHTML = '';
+        expenses.forEach((expense) => {
+            const li = document.createElement('li');
+            li.innerHTML = `${expense.name} - ₹${expense.amount.toFixed(2)}
+            <button class="delete-button" data-expense-id="${expense.id}">Delete</button>
+            `;
+            expenseList.appendChild(li);
+        });
+    }
+    expenseList.addEventListener('click',(e)=>{
+        if(e.target.tagName==="BUTTON"){
+            const expenseId = parseInt(e.target.getAttribute('data-expense-id'));
+            expenses=expenses.filter((expense)=>expense.id!==expenseId);
+            saveExpensetoLocale(expenses);
+            renderExpenses();
+            updateTotal();
+        }
+    })
 
 });
